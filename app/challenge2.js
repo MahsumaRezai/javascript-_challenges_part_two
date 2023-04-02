601ecace73095a2abf49184e35c0cc572aa4b398
Masoumeh Rezai <you@example.com>
2023-04-03 03:38:41 +0430

    s challenge 2

---
+++ app/challenge2.js
@@ -0,0 +1,19 @@
+/*
+Write two functions, isOdd() and isEven(), with a single numeric parameter named
+number. The isOdd() function returns True if number is odd and False if number is even. The
+isEven() function returns the True if number is even and False if number is odd. 
+// answer 
+*/
+
+function checknum(num) {
+    if (num % 2 == 1) {
+        console.log(" your number is odd");
+        return false;
+
+    }
+    else if (num % 2 == 0) {
+        console.log(" ok your number is even");
+        return true;
+    }
+}
+checknum()
