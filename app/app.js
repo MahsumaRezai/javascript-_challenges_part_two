0d0da1e1a612de96560588a17132c7bed942ee55
Masoumeh Rezai <you@example.com>
2023-04-03 03:33:06 +0430

    work on challenge two

---
+++ app/app.js
@@ -0,0 +1,16 @@
+// two 
+
+function checknum(num) {
+    if (num % 2 == 1) {
+        console.log(" your number is odd");
+        return false;
+
+    }
+    else if (num % 2 == 0) {
+        console.log(" ok your number is even");
+        return true;
+
+
+    }
+}
+checknum()
