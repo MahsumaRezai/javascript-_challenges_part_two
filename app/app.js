8e836b5d88ae9b116c0972bd04d9d86d2bcbdf01
Masoumeh Rezai <you@example.com>
2023-04-03 04:35:01 +0430

    work on 5

--- app/app.js
+++ app/app.js
@@ -1,32 +1,17 @@
 /*
-# 4
-Write a fizzBuzz() function with a single integer parameter named upTo. For the numbers 1
-up to and including upTo, the function prints one of four things:
- Prints 'FizzBuzz' if the number is divisible by 3 and 5.
- Prints 'Fizz' if the number is only divisible by 3.
- Prints 'Buzz' if the number is only divisible by 5.
- Prints the number if the number is neither divisible by 3 nor 5.
-answer 
-*/
-function num(n) {
-    if (n % 3 && n % 5) {
-        let text = "FizzBuzz";
-        return text;
+In English, ordinal numerals have suffixes such as the ―th‖ in ―30th‖ or ―nd‖ in ―2nd‖. Write an
+ordinalSuffix() function with an integer parameter named number and returns a string of the
+number with its ordinal suffix. For example, ordinalSuffix(42) should return the string
+'42nd'.
+answer
 
-    }
-    else if (n % 3) {
-        let text1 = "Fizz";
-        return text1;
-    }
-    else if (n % 5) {
-        let text2 = "Buzz";
-        return text2;
-    }
-    else {
-        let text3 = "Prints the number if the number is neither divisible by 3 nor 5.";
-        return text3;
-    }
+*/
+
+function srt(num) {
+    let str = "nd"
+    let concat = num + str;
+    let res = concat.toString();
+    return res;
 
 }
-num();
-
+srt();
