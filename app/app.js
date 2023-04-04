e869ddcf7a5cfc7689cc3ea7c3d18fa3c47ffd5a
Masoumeh Rezai <you@example.com>
2023-04-05 01:40:33 +0430

    challenge 16

--- app/app.js
+++ app/app.js
@@ -2,3 +2,14 @@
 // 11
 // 13
 // 14
+// 16
+function dli() {
+    let arr = [1, 2, 3, 4, 5, 6];
+    let [a, b, c, d, e, f] = arr;
+    for (let i = 0; i < arr.length; i++) {
+        let all = a + b + c + d + e + f
+        console.log(all)
+    }
+
+}
+dli()
