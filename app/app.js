a220da736afbd4b704615ca07a50345c2cb44de7
Masoumeh Rezai <you@example.com>
2023-04-05 01:11:39 +0430

    challenge 12

--- app/app.js
+++ app/app.js
@@ -1,2 +1,24 @@
 // 10
 // 11
+// 12
+function sum(arr, arr1) {
+    if (arr == null && arr1 == null) {
+        return 0;
+
+    }
+    else if (arr != null && arr1 != null) {
+        let s = arr + arr1;
+        return s;
+    }
+}
+sum();
+function product(a, b) {
+    if (a == null && b == null) {
+        return 1;
+    }
+    else if (a != null && b != null) {
+        return a * b;
+    }
+
+}
+product()
