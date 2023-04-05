f9a28a23bb77d4aaec8cb5b0d92e15c178810d90
Masoumeh Rezai <you@example.com>
2023-04-05 23:45:29 +0430

    challenge 21

--- app/app.js
+++ app/app.js
@@ -2,3 +2,23 @@
 // 11
 // 13
 // 14
+
+// 21
+let vild = " 2000 12 30"
+function data() {
+    let exp = new RegExp("[0-9]{4}[0-2]{1,2}[0-9]{1,2}");
+    let d = new Date();
+    let getd = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
+    if (exp.exec(vild)) {
+        return getd;
+
+    }
+    else {
+
+        return false;
+    }
+
+
+}
+data();
+
