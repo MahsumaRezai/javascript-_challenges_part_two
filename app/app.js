32dc2ce4b49e41de6f79a35c0f6c55206405b07a
Masoumeh Rezai <you@example.com>
2023-04-07 00:32:20 +0430

    challenge 27

--- app/app.js
+++ app/app.js
@@ -3,6 +3,20 @@
 // 13
 // 14
 // 23
+// 26 
+// 27 
+function drawRectangle(width, height) {
+    if (width == null && height == null) {
+        return "notthing hte number"
+    }
+    else if (width != null && height != null) {
+        let res = width * height;
+        console.log(res);
+    }
+
+}
+drawRectangle();
+
 
 
 
