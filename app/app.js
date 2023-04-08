9b519b0335e15e07053fcad5681a5df8d2f31597
Masoumeh Rezai <you@example.com>
2023-04-08 23:27:16 +0430

    challenge 33

--- app/app.js
+++ app/app.js
@@ -2,13 +2,22 @@
 // 11
 
 // 26 
-//31 
-function convert(num) {
-    return string.form(num)
+//32 
+
+// 33
+function sp() {
+    let x;
+    let exp = new RegExp("[0-9]{1,3}");
+    let exp2 = new RegExp("[0-9]{4,}");
+    if (exp.test(x)) {
+        return " the number  has one or two parts";
+    }
+    else if (exp2.test(x)) {
+        let s = x.spilt(",");
+        console.log(s, "the number  has many parts")
+    }
 }
-convert()
-
-
+sp()
 
 
 
---
+++ app/challenge31.js
@@ -0,0 +1,5 @@
+//31 
+function convert(num) {
+    return string.form(num)
+}
+convert()
